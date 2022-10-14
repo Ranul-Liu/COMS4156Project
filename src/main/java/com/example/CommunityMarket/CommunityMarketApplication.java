25d5573eefc309d074ae4bb78bcfbef3394ee499
Evstrife <siyuwu0714@gmail.com>
2022-10-14 19:52:44 -0400

    initial springboot skeleton

---
+++ src/main/java/com/example/CommunityMarket/CommunityMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.CommunityMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityMarketApplication.class, args);
+	}
+
+}
