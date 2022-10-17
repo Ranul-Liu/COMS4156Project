75e5f208cfd9ce2e9cade6efd86ca34fcea984b9
Evstrife <siyuwu0714@gmail.com>
2022-10-17 10:44:14 -0400

    added getter & setter for user,item models

--- src/main/java/com/example/CommunityMarket/model/User.java
+++ src/main/java/com/example/CommunityMarket/model/User.java
@@ -1,9 +1,47 @@
 package com.example.CommunityMarket.model;
 
+import java.util.Objects;
+
 public class User {
 
     private int userID;
     private String email;
     private String username;
 
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userID == user.userID && email.equals(user.email) && username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userID, email, username);
+    }
 }
