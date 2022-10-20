e4b45fa4d596508b4667c725b7c595cb6b4aad03
jchenlukas <jiyaochen12@gmail.com>
2022-10-20 16:02:58 -0400

    Modified User class

--- src/main/java/com/example/CommunityMarket/model/User.java
+++ src/main/java/com/example/CommunityMarket/model/User.java
@@ -1,43 +1,114 @@
 package com.example.CommunityMarket.model;
 
 import java.util.Objects;
+import java.util.UUID;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class User {
 
-    private int userID;
+    private UUID userID;
     private String email;
     private String username;
+    private String password;
+    private boolean logInState;
+    private Connection connection;
 
-    public int getUserID() {
-        return userID;
+    public User(String username, String email, String password) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.logInState = false;
+        setUserID();
+        setPassword(password);
+        loadDriver();
+    }
+
+    private void setConnection() {
+        try {
+            this.connection =
+                    DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+                            "user=minty&password=greatsqldb");
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    private void loadDriver() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public void postUser() throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(
+                String.format("INSERT INTO Users (userID, username, password) "
+                        + "values (%s, %s, %s)", this.userID, this.username, this.password));
+        statement.close();
+    }
+
+    public UUID getUserID() {
+        if (this.userID == null) {
+            throw new NullPointerException("userID is null");
+        }
+        else {
+            return this.userID;
+        }
     }
 
     public String getEmail() {
-        return email;
+        return this.email;
     }
 
     public String getUsername() {
-        return username;
+        return this.username;
     }
 
-    public void setUserID(int userID) {
-        this.userID = userID;
+    // private method to initialize a GUID
+    // should only call once
+    private void setUserID() {
+        this.userID = UUID.randomUUID();
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return userID == user.userID && email.equals(user.email) && username.equals(user.username);
+    public void logIn(String password) {
+        if (this.password.equals(password)) {
+            this.logInState = true;
+        }
+        else {
+            throw new IllegalArgumentException("invalid password");
+        }
+    }
+
+    public void logOut() {
+        if (this.logInState) {
+            this.logInState = false;
+        }
+        else {
+            throw new IllegalArgumentException("invalid action");
+        }
+    }
+
+    public boolean equals(User target) {
+        if (target == null) return false;
+        return this.userID == target.userID;
     }
 
     @Override
