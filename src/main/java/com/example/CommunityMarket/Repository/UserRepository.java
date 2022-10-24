834847687cc759e6ec30b0d50e14ab174f30fe99
jchenlukas <jiyaochen12@gmail.com>
2022-10-24 14:07:08 -0400

    Debug User

--- src/main/java/com/example/CommunityMarket/Repository/UserRepository.java
+++ src/main/java/com/example/CommunityMarket/Repository/UserRepository.java
@@ -1,4 +1,3 @@
-/*
 package com.example.CommunityMarket.Repository;
 import com.example.CommunityMarket.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,20 +14,18 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
 
-
-    //get method
-    @Query(value = "select * from Users where ((:userID is NULL or userID = :userID) and\n" +
-            "                          (:username is NULL or username = :username))", nativeQuery = true)
-    List<User> findByTemplate(@Param("userID") String userID,
-                              @Param("username") String username
-    );
+    @Query(value = "select * from user where ((:user_id is NULL or user_id = :user_id) and\n" +
+            "                          (:email is NULL or email = :email) and\n" +
+            "                          (:username is NULL or username LIKE %:username%))", nativeQuery = true)
+    List<User> findByTemplate(@Param("user_id") Integer user_id,
+                              @Param("email") String email,
+                              @Param("username") String username);
 
     //add method
-    @Transactional
-    @Modifying
-    @Query(value = "insert into Users values(:userID,:username,:password)",nativeQuery = true)
-    void insertUser(@Param("userID") String userID,
-                    @Param("username") String username,
-                    @Param("password") String password);
+//    @Transactional
+//    @Modifying
+//    @Query(value = "insert into Users values(:userID,:username,:password)",nativeQuery = true)
+//    void insertUser(@Param("userID") String userID,
+//                    @Param("username") String username,
+//                    @Param("password") String password);
 }
-*/
--- src/main/java/com/example/CommunityMarket/controller/UserController.java
+++ src/main/java/com/example/CommunityMarket/controller/UserController.java
@@ -1,4 +1,3 @@
-/*
 package com.example.CommunityMarket.controller;
 
 import com.example.CommunityMarket.model.User;
@@ -18,27 +17,26 @@ public class UserController {
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public ResponseEntity<?> getUserByTemplate(
-            @RequestParam(value = "userID", required = false) String userID,
-            @RequestParam(value = "username", required = false) String username) {
-
-
-        // verify userID and logged in?
+            @RequestParam(value = "user_id", required = false) Integer user_id,
+            @RequestParam(value = "username", required = false) String username,
+            @RequestParam(value = "email", required = false) String email) {
 
         // get results
-        List<User> result = userService.getUserByTemplate(userID, username);
+        List<User> result = userService.getUserByTemplate(user_id, username, email);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.POST)
     public ResponseEntity<?> postUser(@RequestBody User newUser) {
+        userService.checkPostUser(newUser);
         List<User> result = userService.postUser(newUser);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.PUT)
     public ResponseEntity<?> updateUser(@RequestBody User newUser) {
+        userService.checkPostUser(newUser);
         List<User> result = userService.updateUser(newUser);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
-*/
