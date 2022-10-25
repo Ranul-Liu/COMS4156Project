79360490f7dce2166ba1b97a8d93da885a2a56c6
jchenlukas <jiyaochen12@gmail.com>
2022-10-24 21:32:28 -0400

    Update test

--- src/test/java/com/example/CommunityMarket/Flows/UserTest.java
+++ src/test/java/com/example/CommunityMarket/Flows/UserTest.java
@@ -6,7 +6,6 @@ import com.example.CommunityMarket.Repository.UserRepository;
 import com.example.CommunityMarket.service.UserService;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
