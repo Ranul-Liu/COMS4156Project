c86f7321bee83661e2cd6acd1271de702102348c
Evstrife <siyuwu0714@gmail.com>
2022-10-29 19:55:49 -0400

    more transaction test added

--- src/test/java/com/example/CommunityMarket/Flows/TransactionTest.java
+++ src/test/java/com/example/CommunityMarket/Flows/TransactionTest.java
@@ -110,97 +110,24 @@ public class TransactionTest {
         assertEquals(transactionService.updateTransaction(afterTransaction).get(0).getPrice(), 10);
         assertEquals(transactionService.updateTransaction(afterTransaction).get(0).getQuantity(), 10);
     }
-/*
-    @Test
-    public void testGetItemsByTemplate() {
-        Item newItem = new Item(1,
-                "Sword",
-                "This is a sword",
-                "Attack weapons");
-        Mockito.when(itemRepo.findByTemplate("1",
-                null,
-                null,
-                null)).thenReturn(List.of(newItem));
-        Item result = itemService.getItemByTemplate("1",
-                null,
-                null,
-                null).get(0);
-        assertEquals(result.getId(), result.getId());
-    }
 
-    // Test that the id is correctly updated by postItem method
-    @Test
-    public void testPostItem() {
-
-        // Initialize item before postItem called
-        Item beforeItem = new Item(null,
-                "Sword",
-                "This is a sword",
-                "Attack weapons");
-
-        // Create newly inserted Item
-        Item afterItem = new Item(2,
-                "Sword",
-                "This is a sword",
-                "Attack weapons");
-
-        // save the item
-        Mockito.when(itemRepo.save(beforeItem)).thenReturn(afterItem);
-
-        //assert that the itemId gets correctly updated
-        assertEquals(itemService.postItem(beforeItem).get(0).getId(), 2);
-    }
-
-    //Test that item gets correctly updated
-    @Test
-    public void testUpdateItem() {
-        // Initialize updated item
-        Item beforeItem = new Item(1,
-                "Sword",
-                "This is a sword",
-                "Attack weapons");
-
-        Item updatedItem = new Item(1,
-                "Shield",
-                "This is a shield",
-                "Defense weapons");
-
-        // user exists
-        Mockito.when(itemRepo.findById(String.valueOf(updatedItem.getId()))).thenReturn(Optional.of(updatedItem));
-
-        // save the changes
-        Mockito.when(itemRepo.save(updatedItem)).thenReturn(updatedItem);
-
-        // item exists
-        Mockito.when(itemRepo.existsById("1")).thenReturn(true);
-
-        // save the changes
-        Mockito.when(itemRepo.save(updatedItem)).thenReturn(updatedItem);
-
-        // assert that item gets correctly updated by checking that all data members are equal to the updatedItem
-        assertEquals(itemService.updateItem(updatedItem).get(0).getId(), updatedItem.getId());
-        assertEquals(itemService.updateItem(updatedItem).get(0).getName(), updatedItem.getName());
-        assertEquals(itemService.updateItem(updatedItem).get(0).getDescription(), updatedItem.getDescription());
-        assertEquals(itemService.updateItem(updatedItem).get(0).getCategory(), updatedItem.getCategory());
-
-
-    }
-
-    //Make sure exception raised when the item does not exist
     @Test(expected = IllegalArgumentException.class)
-    public void testExceptionUpdateItem() {
+    public void testExceptionUpdateTransaction() throws ClassNotFoundException, InstantiationException, IllegalArgumentException {
+        //Instantiate updated transacton
+        Transaction afterTransaction = new Transaction(1,
+                "newuser1",
+                "newuser2",
+                2,
+                10,
+                10
+        );
+        //transaction id dos not exist
+        Mockito.when(transactionRepo.existsById((afterTransaction.getTransactionID()))).thenReturn(false);
+        //call update method
+        transactionService.updateTransaction(afterTransaction);
+    }
 
-        // Initialize updated Item
-        Item updatedItem = new Item(10,
-                "Sword",
-                "This is a sword",
-                "Attack weapons");
 
-        // itemID does not exist
-        Mockito.when(itemRepo.existsById("10")).thenReturn(false);
 
-        //call updateItem method
-        itemService.updateItem(updatedItem);
-    }*/
 
 }
