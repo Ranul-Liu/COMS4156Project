9da31e904f67c66a52f7ad4d502e33a0bb3a0ac4
Evstrife <siyuwu0714@gmail.com>
2022-11-26 12:56:05 -0500

    basic auth with jwt finished

--- src/main/java/com/example/CommunityMarket/CommunityMarketApplication.java
+++ src/main/java/com/example/CommunityMarket/CommunityMarketApplication.java
@@ -1,14 +1,17 @@
 package com.example.CommunityMarket;
 
+import com.example.CommunityMarket.config.RsaKeyProperties;
 import com.example.CommunityMarket.model.Item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 @RestController
 public class CommunityMarketApplication {
